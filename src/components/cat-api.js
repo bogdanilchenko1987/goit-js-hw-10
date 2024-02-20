aca5efff9404f2a80eab63f3cad94444ef2d4eba
bogdanilchenko1987 <bogdan_ilchenko@ukr.net>
2024-02-20 15:33:52 +0200

    4 commit

---
+++ src/components/cat-api.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const BASE_URL = 'https://api.thecatapi.com/v1';
+axios.defaults.headers.common['x-api-key'] =
+  'live_0XRpC8Po01PXAQJmKptYMksyFdVeOuaR6d7U8pG03zd8V4rFBYCgYiOSFgFVRDWF';
+
+function fetchBreeds() {
+  return axios
+    .get(`${BASE_URL}/breeds`)
+    .then(response => {
+      return response;
+    })
+    .catch(err => {
+      console.log(err);
+      Notify.failure('Oops! Something went wrong! Try reloading the page!', {
+        position: 'center-top',
+        timeout: 1500,
+      });
+    });
+}
+
+function fetchCatByBreed(breedId) {
+  return axios
+    .get(`${BASE_URL}/images11/${breedId}`)
+    .then(response => {
+      if (response.status !== 200) {
+        // refs.error.hidden = false;
+        Notify.failure('Oops! Something went wrong! Try reloading the page!', {
+          position: 'center-top',
+          timeout: 1500,
+        });
+      }
+
+      return response.data;
+    })
+    .catch(err => {
+      console.log(err);
+      refs.loader.hidden = true;
+      Notify.failure('Oops! Something went wrong! Try reloading the page!', {
+        position: 'center-top',
+        timeout: 1500,
+      });
+    });
+}
+
+export { fetchBreeds, fetchCatByBreed };
--- src/index.js
+++ src/index.js
@@ -1,77 +1,58 @@
-import axios from 'axios';
 import './css/common.css';
 
-axios.defaults.headers.common['x-api-key'] =
-  'live_0XRpC8Po01PXAQJmKptYMksyFdVeOuaR6d7U8pG03zd8V4rFBYCgYiOSFgFVRDWF';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-// { 'x-api-key' : 'YOUR_API_KEY' }
+import { createMarkupDiv } from './helpers/createMarkupDiv';
+import { createMarkupSelect } from './helpers/createMarkupSelect';
+import { fetchBreeds, fetchCatByBreed } from './components/cat-api';
 
 const refs = {
   catSelect: document.querySelector('.breed-select'),
   catDiv: document.querySelector('.cat-info'),
+  loader: document.querySelector('.loading'),
 };
 
-const BASE_URL = 'https://api.thecatapi.com/v1';
-const API_KEY =
-  'live_0XRpC8Po01PXAQJmKptYMksyFdVeOuaR6d7U8pG03zd8V4rFBYCgYiOSFgFVRDWF';
+refs.loader.hidden = false;
 
-const params = {
-  header: {
-    'x-api-key': 'API_KEY',
-  },
-};
-
-function fetchBreeds() {
-  const url = `${BASE_URL}/breeds`;
-  return fetch(url, params).then(response => {
-    return response.json();
-  });
-}
-
-fetchBreeds()
-  .then(data => {
-    console.log(data);
-    refs.catSelect.insertAdjacentHTML('beforeend', createMarkupSelect(data));
-  })
-  .catch(err => console.log(err));
-
-function createMarkupSelect(arr) {
-  return arr
-    .map(({ name, id }) => `<option value="${id}">${name}</option>`)
-    .join('');
-}
-
-function createMarkupDiv(item) {
-  return `<img src="https://cdn2.thecatapi.com/images/${item.reference_image_id}.jpg" alt="${item.name} ">
-  <div class ="cat-description">  <h2>${item.name}</h2>
-    <p>${item.description}</p>
-    <p><span>Temperament : </span>${item.temperament}</p>
-  </div>  `;
-}
-
-function fetchCatByBreed(breedId) {
-  const url = `${BASE_URL}/images/search?breed_ids=${breedId}`;
-  return fetch(url, params).then(response => {
-    return response.json();
-  });
-}
+setTimeout(() => {
+  fetchBreeds()
+    .then(data => {
+      refs.loader.hidden = true;
+      refs.catSelect.hidden = false;
+      refs.catSelect.insertAdjacentHTML(
+        'beforeend',
+        createMarkupSelect(data.data)
+      );
+    })
+    .catch(err => {
+      console.log(err);
+      refs.catSelect.hidden = true;
+    });
+}, 300);
 
 refs.catSelect.addEventListener('input', onSelect);
 
 function onSelect(e) {
   const currentCatId = e.currentTarget.value;
 
-  fetchCatByBreed(currentCatId)
-    .then(data => console.log(data))
-    .catch(err => console.log(err));
+  refs.loader.hidden = false;
+  refs.catDiv.innerHTML = '';
 
-  fetchBreeds()
-    .then(data => {
-      data.forEach(item => {
-        if (item.id === currentCatId) {
-          refs.catDiv.innerHTML = createMarkupDiv(item);
+  setTimeout(() => {
+    fetchCatByBreed(currentCatId)
+      .then(data => {
+        if (currentCatId === data.id) {
+          refs.catDiv.innerHTML = createMarkupDiv(data);
         }
+        refs.loader.hidden = true;
+      })
+      .catch(err => {
+        console.log(err);
+        refs.loader.hidden = true;
+        Notify.failure('Oops! Something went wrong! Try reloading the page!', {
+          position: 'center-top',
+          timeout: 1500,
+        });
       });
-    })
-    .catch(err => console.log(err));
+  }, 300);
 }
